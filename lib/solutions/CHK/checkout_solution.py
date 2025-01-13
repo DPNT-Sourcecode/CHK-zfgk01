be4f78f37ec16b9ef3f4d3d8ee5ec9726f585d9c
sbx_user1051 <sbx_user1051@169.254.160.214>
2025-01-13 22:41:24 +0000

    Mon Jan 13 22:41:24 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import Optional
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -23,7 +24,7 @@ def is_valid_sku(sku: str) -> bool:
     return sku in item_prices.keys()
 
 
-def _build_checkout_basket(skus_array: list[str]) -> dict[str, int]:
+def _build_checkout_basket(skus_array: list[str]) -> Optional[dict[str, int]]:
     basket = defaultdict(int)
     for sku in skus_array:
         if not is_valid_sku(sku):
@@ -44,31 +45,23 @@ def checkout(skus: str) -> int:
     total_price = 0
     skus_array = _build_skus_array(skus)
     basket = _build_checkout_basket(skus_array)
-    if not basket:
+    if basket is None:
         return -1
-    
-    for item, count in basket:
-        if not is_valid_sku(sku):
-            return -1
 
-        if len(sku) > 1:
-            multiply = sku[:-1]
-            item = sku[-1:]
-            # offer = special_offers[item]
-            # if offer:
-            #     offer_units, offer_price = offer
-            #     offer_multiply, regular_price_multiply = divmod(multiply, offer_units)
-            #     total_price += (offer_multiply * offer_price)
-            #     total_price += (item_prices[item] * regular_price_multiply)
-            # else:
-            #     total_price += item_prices[item]
+    for item, item_count in basket:
+        offer = special_offers[item]
+        if offer:
+            offer_units, offer_price = offer
+            offer_multiply, regular_price_multiply = divmod(item_count, offer_units)
+            total_price += (offer_multiply * offer_price)
+            total_price += (item_prices[item] * regular_price_multiply)
         else:
-            total_price += item_prices[sku]
+            total_price += item_count * item_prices[item]
 
     return total_price
 
 
-def _build_skus_array(skus):
+def _build_skus_array(skus: str) -> list[str]:
     i = 0
     skus_array = []
     current_sku = ''
@@ -80,7 +73,9 @@ def _build_skus_array(skus):
         i += 1
     if current_sku:
         skus_array.append(current_sku)
+
     return skus_array
+
 
 
 
