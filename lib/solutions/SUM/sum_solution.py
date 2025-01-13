a518885b001defc2fce520e04bd366c665095dc1
sbx_user1051 <sbx_user1051@169.254.160.214>
2025-01-13 21:51:53 +0000

    Mon Jan 13 21:51:53 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,21 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+class IllegalParameter(Exception):
+
+    def __init__(self, error_msg: str, *args: object) -> None:
+        super().__init__(*args)
+        self.error_msg = error_msg
+
+    def __repr__(self) -> str:
+        return f'Error: {self.error_msg}'
+
+
+LEGAL_RANGE_INPUT = list(range(100 + 1)) # range is not inclusive
+
+
+def compute(x: int, y: int) -> int:
+    if not isinstance(x, int) or not isinstance(y, int):
+        raise IllegalParameter(f'x or y are illegal! x={x} y={y}')
+    if x not in LEGAL_RANGE_INPUT or y not in LEGAL_RANGE_INPUT:
+        raise IllegalParameter(f'x or y are out of range! x={x} y={y}')
+    return x + y
+
