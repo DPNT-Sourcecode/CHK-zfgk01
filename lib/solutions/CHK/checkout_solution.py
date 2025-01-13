785ce0311b5406fcec7612313d7f3c4a4b8fbe03
sbx_user1051 <sbx_user1051@169.254.160.214>
2025-01-13 22:35:35 +0000

    Mon Jan 13 22:35:35 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -23,8 +23,45 @@ def is_valid_sku(sku: str) -> bool:
     return sku in item_prices.keys()
 
 
+def _build_checkout_basket(skus_array: list[str]) -> dict[str, int]:
+    defaultdict()
+    for sku in skus_array:
+        if not is_valid_sku(sku):
+            return None
+
+        if len(sku) > 1:
+            multiply = sku[:-1]
+            item = sku[-1:]
+
+
+
 def checkout(skus: str) -> int:
     total_price = 0
+    skus_array = _build_skus_array(skus)
+
+    basket = _build_checkout_basket(skus_array)
+    for sku in skus_array:
+        if not is_valid_sku(sku):
+            return -1
+
+        if len(sku) > 1:
+            multiply = sku[:-1]
+            item = sku[-1:]
+            # offer = special_offers[item]
+            # if offer:
+            #     offer_units, offer_price = offer
+            #     offer_multiply, regular_price_multiply = divmod(multiply, offer_units)
+            #     total_price += (offer_multiply * offer_price)
+            #     total_price += (item_prices[item] * regular_price_multiply)
+            # else:
+            #     total_price += item_prices[item]
+        else:
+            total_price += item_prices[sku]
+
+    return total_price
+
+
+def _build_skus_array(skus):
     i = 0
     skus_array = []
     current_sku = ''
@@ -36,25 +73,6 @@ def checkout(skus: str) -> int:
         i += 1
     if current_sku:
         skus_array.append(current_sku)
-
-    for sku in skus_array:
-        if not is_valid_sku(sku):
-            return -1
-
-        if len(sku) > 1:
-            multiply = sku[:-1]
-            item = sku[-1:]
-            offer = special_offers[item]
-            if offer:
-                offer_units, offer_price = offer
-                offer_multiply, regular_price_multiply = divmod(multiply, offer_units)
-                total_price += (offer_multiply * offer_price)
-                total_price += (item_prices[item] * regular_price_multiply)
-            else:
-                total_price += item_prices[item]
-        else:
-            total_price += item_prices[sku]
-
-    return total_price
+    return skus_array
 
 
