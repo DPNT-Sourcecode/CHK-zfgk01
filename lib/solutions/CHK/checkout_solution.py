70e6900ca9791f5fb6c97d5b82020e4b4a44bedc
sbx_user1051 <sbx_user1051@169.254.160.214>
2025-01-13 22:19:26 +0000

    Mon Jan 13 22:19:26 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,17 +9,23 @@ item_prices = {
     'D': 15,
 }
 
+
 special_offers = {
     'A': (3, 130),
     'B': (2, 45),
 }
+
+
+def is_valid(sku: str) -> bool:
+    if len(sku) > 1:
+        sku = sku[-1:]
 def checkout(skus: str) -> int:
     skus_array = skus.split()
     total_price = 0
     for sku in skus_array:
-        multiply = sku[:-1]
-        item = sku[-1:]
-        if multiply:
+        if len(sku) > 1:
+            multiply = sku[:-1]
+            item = sku[-1:]
             offer = special_offers[item]
             if offer:
                 offer_units, offer_price = offer
@@ -28,7 +34,5 @@ def checkout(skus: str) -> int:
                 total_price += (item_prices[item] * regular_price_multiply)
             else:
                 total_price += item_prices[item]
-
-
-
-
+        else:
+            total_price += item_prices[item]
